10f7e49b1316d4df0abe2d9a83cb4bcbb40ee97b
Medscootsman <murraylyne2@Gmail.com>
2016-10-31 16:07:23 +0000

    db

--- inc/scripts/dbconnect.php
+++ inc/scripts/dbconnect.php
@@ -2,8 +2,8 @@
 $servername = "eu-cdbr-azure-north-e.cloudapp.net";
 $username = "baa0a9a8041f1c";
 $password = "ea0c77dc";
-
-$db = new mysqli($servername, $username, $password);
+$database = "acsm_eeffc4063160a9f";
+$db = new mysqli($servername, $username, $password, $database);
 
 if ($db->connect_error) {
     die("connection failed" . $db->connect_error);
